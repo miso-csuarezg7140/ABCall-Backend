b86c7d765e57209a2e15ee4ac2417f4369c82900
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2024-10-09 21:09:37 -0500

    Commit inicial de MS ABCall.

---
+++ MSIncidentes/src/test/java/com/abcall/incidentes/IncidentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcall.incidentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
