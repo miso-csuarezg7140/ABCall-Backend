f555d3f2e5b945c79b570165df6bf55c33b84f54
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2024-10-27 20:31:04 -0500

    Ajuste mapper agentes

--- MSAgentes/src/main/java/com/abcall/agentes/persistence/mappers/AgenteMapper.java
+++ MSAgentes/src/main/java/com/abcall/agentes/persistence/mappers/AgenteMapper.java
@@ -4,15 +4,12 @@ import com.abcall.agentes.domain.dto.AgenteDto;
 import com.abcall.agentes.persistence.entity.Agente;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AgenteMapper {
 
-    @Mapping(source = "agentePK.tipoDocumento", target = "tipoDocumento")
-    @Mapping(source = "agentePK.numeroDocumento", target = "numeroDocumento")
     AgenteDto toDto(Agente agente);
 
     List<AgenteDto> toDtoList(List<Agente> agenteList);
