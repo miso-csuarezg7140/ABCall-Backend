74822b040444cf2a83a39c99d2430025cb59a609
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2025-04-06 20:04:42 -0500

    Fix Sonar.

--- MSClientes/src/main/java/com/abcall/clientes/config/SecurityConfig.java
+++ MSClientes/src/main/java/com/abcall/clientes/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,11 +20,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/ping", "/authenticate").permitAll()
-                    .anyRequest().authenticated()
-            );
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/ping", "/authenticate"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/ping", "/authenticate").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
