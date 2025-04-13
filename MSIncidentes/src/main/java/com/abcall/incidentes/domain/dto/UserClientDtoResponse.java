462d2c8053ab4437e05d5e3bccaddf93a5bcf85b
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2025-04-13 14:41:41 -0500

    Refactor IncidenteServiceImplTest to improve user client validation and response handling; add UserClientDtoResponse constructor

--- MSIncidentes/src/main/java/com/abcall/incidentes/domain/dto/UserClientDtoResponse.java
+++ MSIncidentes/src/main/java/com/abcall/incidentes/domain/dto/UserClientDtoResponse.java
@@ -2,11 +2,13 @@ package com.abcall.incidentes.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserClientDtoResponse {
 
     private String documentTypeUser;
