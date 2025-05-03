f3c41d9bd303ab3c59ada23f99f07e30736b88f7
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2025-05-03 11:32:51 -0500

    Refactor health check configurations and remove unused reporter service

--- MSReporteria/src/main/java/com/abcall/reporteria/persistence/repository/jpa/ReporteriaRepositoryJpa.java
+++
@@ -1,7 +0,0 @@
-package com.abcall.reporteria.persistence.repository.jpa;
-
-import com.abcall.reporteria.persistence.entity.Reporteria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReporteriaRepositoryJpa extends JpaRepository<Reporteria, Long> {
-}
