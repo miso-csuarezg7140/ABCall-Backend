58a7030d3766c88b8616a9646bdc3bfaa50e5ebd
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2025-05-18 12:59:08 -0500

    Update SecurityConfig.java

--- MSClientes/src/main/java/com/abcall/clientes/security/SecurityConfig.java
+++ MSClientes/src/main/java/com/abcall/clientes/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/ping",
                         "/swagger-ui.html/**", "/v3/api-docs/**", "/registrar", "/autenticar", "/tiposDocumento",
-                        "/listar", "/validar Usuario"))
+                        "/listar", "/validarUsuario"))
                 .cors(AbstractHttpConfigurer::disable)
                 .exceptionHandling(handling -> handling
                         .authenticationEntryPoint((
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/ping", "/swagger-ui.html/**", "/v3/api-docs/**", "/registrar",
-                                "/autenticar", "/tiposDocumento", "/listar", "/validar Usuario" )
+                                "/autenticar", "/tiposDocumento", "/listar", "/validarUsuario" )
                         .permitAll().anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
